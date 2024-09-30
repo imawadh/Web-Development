9ee19266e73341ca99d5dede636d347d634c3ef4
Awadh Kishor Singh <148574291+imawadh@users.noreply.github.com>
2024-09-30 11:56:06 +0530

    CSS Pseudo Selector

--- PWIOI/Projects/Project 1/script.js
+++
@@ -1,6 +0,0 @@
-document.body.addEventListener("click",function()
-{
-    const random_color = "#" + Math.floor(Math.random() * 1677215).toString();
-    console.log(random_color);
-    document.body.style.backgroundColor = random_color;
-})
