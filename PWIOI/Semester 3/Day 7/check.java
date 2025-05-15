06a88eef36b0c826fa83a9ecb6e9c0cfde1599da
Awadh Kishor Singh <awadhkishorsingh241@gmail.com>
2025-05-15 10:05:54 +0530

    Day 7 of JS

---
+++ PWIOI/Semester 3/Day 7/check.java
@@ -0,0 +1,10 @@
+class check{
+    static void add(int a, int b){
+        System.out.println(a+b);
+    }
+    public static void main(String[] args) {
+        // System.out.println(add(10,20));
+        // WE cann't print a void function 
+        add(10,20);
+    }
+}
---
+++ PWIOI/Semester 3/Day 7/check.js
@@ -0,0 +1,4 @@
+function add(a,b){
+    console.log(a+b)
+}
+console.log(add(10,20));
